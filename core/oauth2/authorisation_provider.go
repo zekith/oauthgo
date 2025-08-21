89e837fae3f7fd54c09beeea5d3c2f26a8a9c13b
rakesh-eltropy <>
2025-08-21 22:28:06 +0530

    Add OAuth2 authorization provider interface and related types

---
+++ core/oauth2/authorisation_provider.go
@@ -0,0 +1,42 @@
+package oauthgoauth2
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+// AuthOptions are the options for generating an authentication URL.
+type AuthOptions struct {
+	RedirectURL string
+	Scopes      []string
+	Prompt      string
+	LoginHint   string
+	UsePKCE     bool
+	Extras      map[string]string
+}
+
+// Session represents an OAuth2 session containing an access token, refresh token, and ID token.
+type Session struct {
+	Provider     string
+	AccessToken  string
+	RefreshToken string
+	IDToken      string // optional; empty in pure OAuth2
+	TokenType    string
+	Expiry       time.Time
+	Raw          map[string]any
+}
+
+// AuthorisationProvider is an OAuth2 provider that needs to be implemented by a provider.
+type AuthorisationProvider interface {
+	// Name returns the name of the provider.
+	Name() string
+	// AuthURL returns the URL to redirect the user to for authentication.
+	AuthURL(ctx context.Context, r *http.Request, opts AuthOptions) (url string, opaqueState string, err error)
+	// Exchange exchanges an authorization code for an access token.
+	Exchange(ctx context.Context, r *http.Request, code string, opaqueState string) (*Session, error)
+	// Refresh refreshes an access token.
+	Refresh(ctx context.Context, refreshToken string) (*Session, error)
+	// Revoke revokes a token.
+	Revoke(ctx context.Context, token string) error
+}
--- core/provider/provider.go
+++ core/provider/provider.go
@@ -3,58 +3,47 @@ package oauthgoprovider
 import (
 	"context"
 	"net/http"
-	"time"
 
-	"github.com/zekith/oauthgo/core/types"
+	oauthgoauth2 "github.com/zekith/oauthgo/core/oauth2"
+	oauthgooidc "github.com/zekith/oauthgo/core/oidc"
 )
 
-// AuthOptions are the options for generating an authentication URL.
-type AuthOptions struct {
-	RedirectURL string
-	Scopes      []string
-	Prompt      string
-	LoginHint   string
-	UsePKCE     bool
-	Extras      map[string]string
-}
-
-// Session represents an OAuth2 session containing an access token, refresh token, and ID token.
-type Session struct {
-	Provider     string
-	AccessToken  string
-	RefreshToken string
-	IDToken      string
-	TokenType    string
-	Expiry       time.Time
-	Raw          map[string]any
-}
-
-// User represents details that can be extracted from an ID token.
-type User struct {
-	Subject       string
-	Email         string
-	EmailVerified bool
-	Name          string
-	GivenName     string
-	FamilyName    string
-	Picture       string
-	Locale        *oauthgotypes.Locale
-	Attributes    map[string]string
-	RawProfile    map[string]any
-}
-
-// Provider is an OAuth2 provider that needs to be implemented by a provider.
 type Provider interface {
-	// AuthURL returns the URL to redirect the user to for authentication.
-	AuthURL(ctx context.Context, r *http.Request, opts AuthOptions) (url string, opaqueState string, err error)
-	// Exchange exchanges an authorization code for an access token.
-	Exchange(ctx context.Context, r *http.Request, code string, opaqueState string) (*Session, error)
-	// Refresh refreshes an access token.
-	Refresh(ctx context.Context, refreshToken string) (*Session, error)
-	// UserInfo returns the user information.
-	UserInfo(ctx context.Context, accessToken, idToken string) (*User, error)
-	// Revoke revokes a token.
-	Revoke(ctx context.Context, token string) error
-	// Name returns the name of the provider.
 	Name() string
+	AuthURL(ctx context.Context, r *http.Request, opts oauthgoauth2.AuthOptions) (string, string, error)
+	Exchange(ctx context.Context, r *http.Request, code, opaqueState string) (*oauthgoauth2.Session, error)
+	Refresh(ctx context.Context, refreshToken string) (*oauthgoauth2.Session, error)
+	Revoke(ctx context.Context, token string) error
+	UserInfo(ctx context.Context, accessToken, idToken string) (*oauthgooidc.User, error)
+}
+
+type AuthFacade struct {
+	authorisationProvider oauthgoauth2.AuthorisationProvider
+	identityProvider      oauthgooidc.IdentityProvider // nil for OAuth2-only providers
+}
+
+func NewAuthFacade(authorisationProvider oauthgoauth2.AuthorisationProvider, identityProvider oauthgooidc.IdentityProvider) *AuthFacade {
+	return &AuthFacade{authorisationProvider: authorisationProvider, identityProvider: identityProvider}
+}
+
+func (f *AuthFacade) Name() string { return f.authorisationProvider.Name() }
+
+func (f *AuthFacade) AuthURL(ctx context.Context, r *http.Request, opts oauthgoauth2.AuthOptions) (string, string, error) {
+	return f.authorisationProvider.AuthURL(ctx, r, opts)
+}
+func (f *AuthFacade) Exchange(ctx context.Context, r *http.Request, code, state string) (*oauthgoauth2.Session, error) {
+	return f.authorisationProvider.Exchange(ctx, r, code, state)
+}
+func (f *AuthFacade) Refresh(ctx context.Context, refreshToken string) (*oauthgoauth2.Session, error) {
+	return f.authorisationProvider.Refresh(ctx, refreshToken)
+}
+func (f *AuthFacade) Revoke(ctx context.Context, token string) error {
+	return f.authorisationProvider.Revoke(ctx, token)
+}
+func (f *AuthFacade) UserInfo(ctx context.Context, accessToken, idToken string) (*oauthgooidc.User, error) {
+	if f.identityProvider == nil {
+		return &oauthgooidc.User{}, nil
+	}
+	_ = f.identityProvider.VerifyIDToken(ctx, idToken) // ignore if empty
+	return f.identityProvider.UserInfo(ctx, accessToken, idToken)
 }
